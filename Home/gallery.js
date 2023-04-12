a6b7310c3783bcbca50199218b1f1c006afa0597
Jay Mast <j.mast.finance@gmail.com>
2023-04-12 12:20:03 -0500

    added gallery, contact, fixed footer

---
+++ Home/gallery.js
@@ -0,0 +1,47 @@
+// Open the modal
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+  }
+  
+  // Close the modal
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+  
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    const slides = document.getElementsByClassName('mySlides');
+    const dots = document.getElementsByClassName('demo');
+    const captionText = document.getElementById('caption');
+    
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+    
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+  }
